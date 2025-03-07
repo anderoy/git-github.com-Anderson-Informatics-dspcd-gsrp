379d63fae98c50c02162a0c01a19b50204d1856a
Eric Anderson <eric@booksuey.com>
2025-03-07 16:52:47 +0100

    remove purge config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,14 @@ module.exports = {
   plugins: [],
   purge: {
     //enable remove unused CSS only in production
-    enabled: process.env.NODE_ENV === "production",
+    //enabled: process.env.NODE_ENV === "production",
     //any file containing the reference of CSS styles by class name.
-    content: [
-      "components/**/*.vue",
-      "layouts/**/*.vue",
-      "pages/**/*.vue",
-      "plugins/**/*.js",
-      "nuxt.config.js",
-    ],
+    //content: [
+    //  "components/**/*.vue",
+    //  "layouts/**/*.vue",
+    //  "pages/**/*.vue",
+    //  "plugins/**/*.js",
+    //  "nuxt.config.js",
+    //],
   },
 };
